0082b76e17ba03104edd872fc65d2ec39ff248dd
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-06 19:34:20 -0500

    Installed nodemon dependency and added new features to example_4 to show nodemon functionality

--- playground/example_4.js
+++ playground/example_4.js
@@ -3,12 +3,16 @@ const chalk = require('chalk');
 
 console.log(chalk.green('Success!'));
 console.log(chalk.red('Error'));
+console.log(chalk.yellow('Syntax error \n'));
 
 console.log(chalk.green.inverse('Success!'));
 console.log(chalk.red.inverse('Error'));
+console.log(chalk.yellow.inverse('Syntax error \n'));
 
 console.log(chalk.green.bold('Success!'));
 console.log(chalk.red.bold('Error'));
+console.log(chalk.yellow.bold('Syntax error \n'));
 
 console.log(chalk.green.inverse.bold('Success!'));
 console.log(chalk.red.inverse.bold('Error'));
+console.log(chalk.yellow.inverse.bold('Syntax error \n'));
