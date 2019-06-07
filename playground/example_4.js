02a93cf78471f103cc1ffb036b0babc509dd972e
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-06 19:29:24 -0500

    Refactored the example to use more features of chalk dependency

--- playground/example_4.js
+++ playground/example_4.js
@@ -3,3 +3,12 @@ const chalk = require('chalk');
 
 console.log(chalk.green('Success!'));
 console.log(chalk.red('Error'));
+
+console.log(chalk.green.inverse('Success!'));
+console.log(chalk.red.inverse('Error'));
+
+console.log(chalk.green.bold('Success!'));
+console.log(chalk.red.bold('Error'));
+
+console.log(chalk.green.inverse.bold('Success!'));
+console.log(chalk.red.inverse.bold('Error'));
