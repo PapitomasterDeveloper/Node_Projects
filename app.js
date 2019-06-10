f81602e1e329b22bb5d3dba5d4fd951cdc892bc5
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-10 14:48:21 -0500

    Added handling for errors

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ request({url: url, json: true},(error, response) => {
 	//const data = JSON.parse(response.body);
 	//console.log(data.currently);
 	//console.log(response.body.currently);
-	console.log(' ' + response.body.daily.data[0].summary + '\n It is currently ' + response.body.currently.temperature + ' degrees out.\n There is a ' + response.body.currently.precipProbability + ' % chance of rain.');
+	if(error){
+		console.log('Unable to connect to weather service');
+	} else if(response.body.error){
+		console.log('Unable to find location');
+	} else {
+		console.log(' ' + response.body.daily.data[0].summary)
+		console.log('\n It is currently ' + response.body.currently.temperature + ' degrees out.\n');
+	}
 })
 
 const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGFwaXRvbWFzdGVyIiwiYSI6ImNqd3FxMmt6dzFkeXM0Y29kMXQ3cG10MzYifQ.AA6MnvhFvThQl2J-6pefKw&limit=1';
 
 request({url: geocodeURL, json: true}, (error, response) => {
-	const latitude = response.body.features[0].center[1];
-	const longitude = response.body.features[0].center[0];
-	console.log(latitude, longitude);
+	if(error) {
+		console.log('Unable to connect to location services!');
+	} else if(response.body.features.length === 0) {
+		console.log('Unable to find location, try another search.');
+	} else {
+		const latitude = response.body.features[0].center[1];
+        	const longitude = response.body.features[0].center[0];
+        	console.log(latitude, longitude);
+	}
 });
 
