67b01b6edadd39b29a5a5223b6ce912eb811cbb6
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-10 12:52:56 -0500

    Consumed from API and parsed the output

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/1fa1a6fd5a0adce4becbbbdae63f6755/37.8267,-122.4233';
+
+request({url: url},(error, response) => {
+	const data = JSON.parse(response.body);
+	console.log(data.currently);
+})
