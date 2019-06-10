98563097ca349ae3dc11b2045d721c709ae062fc
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-10 14:05:27 -0500

    Made a second API request for the latitude and longitude values

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ request({url: url, json: true},(error, response) => {
 	console.log(' ' + response.body.daily.data[0].summary + '\n It is currently ' + response.body.currently.temperature + ' degrees out.\n There is a ' + response.body.currently.precipProbability + ' % chance of rain.');
 })
 
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGFwaXRvbWFzdGVyIiwiYSI6ImNqd3FxMmt6dzFkeXM0Y29kMXQ3cG10MzYifQ.AA6MnvhFvThQl2J-6pefKw&limit=1';
+
+request({url: geocodeURL, json: true}, (error, response) => {
+	const latitude = response.body.features[0].center[1];
+	const longitude = response.body.features[0].center[0];
+	console.log(latitude, longitude);
+});
+
