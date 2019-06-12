1bbc679cf8d15b9b63bb9fd4b830cf74353060c6
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-11 20:10:08 -0500

    Refactored the code with ES6 feature object property shortcut, again

--- utils/geocode.js
+++ utils/geocode.js
@@ -4,16 +4,16 @@ const geocode = (address, callback) => {
 
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoicGFwaXRvbWFzdGVyIiwiYSI6ImNqd3FxMmt6dzFkeXM0Y29kMXQ3cG10MzYifQ.AA6MnvhFvThQl2J-6pefKw&limit=1';
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (response.body.features === undefined || response.body.features.length === 0) {
+        } else if (body.features === undefined || body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
     })
