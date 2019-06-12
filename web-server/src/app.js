b8d60694921d8d06c61a13a1307e7ca5e8595cba
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-12 01:16:22 -0500

    Installed handlebars dependency to serving up hbs content, BUG

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,7 +8,12 @@ const path = require('path');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+	res.render('index');
+});
 
 app.get('/weather', (req, res) => {
 	res.send({
