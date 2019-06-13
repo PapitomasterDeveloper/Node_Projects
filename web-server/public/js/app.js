ad4912d86651560f96922eee217e3f0669702430
PapitomasterDeveloper <papitomasterdeveloper@gmail.com>
2019-06-13 01:36:00 -0500

    Rendered the fetched search results below the form in the root endpoint

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -8,6 +8,8 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -17,10 +19,13 @@ weatherForm.addEventListener('submit', (e) => {
 	fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     	response.json().then((data) => {
         	if(data.error) {
-            		console.log(data.error);
+            		//console.log(data.error);
+			messageOne.textContent = data.error;
         	} else {
-            		console.log(data.location);
-            		console.log(data.forecast);
+			messageOne.textContent = data.location;
+			messageTwo.textContent = data.forecast;
+            		//console.log(data.location);
+            		//console.log(data.forecast);
         	}
     	})
 })
